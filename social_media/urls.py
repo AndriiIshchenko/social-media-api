e0d93512fffe754fc76ae250cfdfcdb1f369b780
AndriiIshchenko <iscenkoa@gmail.com>
2024-11-25 16:49:22 +0200

    chor: remove unused urls

--- social_media/urls.py
+++ social_media/urls.py
@@ -14,25 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView
-)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-      path("api/user/", include("user.urls", namespace="user")),
+    path("admin/", admin.site.urls),
+    path("api/user/", include("user.urls", namespace="user")),
     path(
         "api/posts/",
         include("posts.urls", namespace="posts"),
     ),
-    path('api/user/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/user/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/user/token/verify/', TokenVerifyView.as_view(), name='token_verify')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
