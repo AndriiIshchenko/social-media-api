df25e352802f2c6a4e490c779fec973a52fcb159
AndriiIshchenko <iscenkoa@gmail.com>
2024-11-26 20:00:29 +0200

    feat: add url for UserProfile

--- posts/urls.py
+++ posts/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from posts.views import LikeViewSet, PostViewSet
+from posts.views import LikeViewSet, PostViewSet, UserProfileViewSet
 
 
 router = routers.DefaultRouter()
 
 router.register("likes", LikeViewSet)
 router.register("posts", PostViewSet)
+router.register("profiles", UserProfileViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
