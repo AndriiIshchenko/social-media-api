95d01e9ca74c77423f607c34d1dfcceabf44cb7e
AndriiIshchenko <iscenkoa@gmail.com>
2024-11-27 18:27:41 +0200

    feat: add UserProfile, Comment to admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
 from .models import (
+    Comment,
     Like,
     Post,
+    UserProfile,
 )
 
 admin.site.register(Like)
 admin.site.register(Post)
+admin.site.register(UserProfile)
+admin.site.register(Comment)
